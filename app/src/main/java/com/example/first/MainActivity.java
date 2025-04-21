735b16e5f2126558a7f21c252f74cdaccf1c0478
evgenyi ulyankin <ulyankin1986@gmail.com>
2025-04-21 21:38:46 +0300

    1first commit

--- app/src/main/java/com/example/first/MainActivity.java
+++ app/src/main/java/com/example/first/MainActivity.java
@@ -13,8 +13,12 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView textSample;
-    private Button secondButton;
+
+//    private TextView textSample;
+//    private Button secondButton;
+
+    private TextView text_hw3;
+    private Button two_button18;
 
 
     @Override
@@ -28,22 +32,44 @@ public class MainActivity extends AppCompatActivity {
 //        setContentView(R.layout.table_layout);
 //        setContentView(R.layout.frame_layout);
 //        setContentView(R.layout.scroll_view);
-        setContentView(R.layout.third_layout);
+//        setContentView(R.layout.third_layout);
+        setContentView(R.layout.home_work3btn_layout);
 
-        textSample = findViewById(R.id.textSample);
-        secondButton = findViewById(R.id.secondButton);
+        text_hw3 = findViewById(R.id.text_hw3);
+        two_button18 = findViewById(R.id.two_button18);
 
-        secondButton.setOnClickListener(new View.OnClickListener() {
+        two_button18.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textSample.setText("Новое приветствие");
-                textSample.setTextSize(40);
+                text_hw3.setText("ХОРОШО!!!");
+
             }
         });
 
+
+         //   @Override
+
+
+//        textSample = findViewById(R.id.textSample);
+//        secondButton = findViewById(R.id.secondButton);
+
+//        secondButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                textSample.setText("Новое приветствие");
+//                textSample.setTextSize(40);
+//            }
+//        });
+//
+    }
+    public void greeting(View view) {
+        text_hw3.setText("Всё будет");
     }
 
-    public void sayHello (View view) {
-        textSample.setText("Привет");
+
+
     }
-}
+//    public void sayHello (View view) {
+//        textSample.setText("Привет");
+//
+//    }
